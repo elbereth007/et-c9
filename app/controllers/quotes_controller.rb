b6d9f6043a293d080d0889c22cf1b4e4fc5613e5
Victor Christensen <victor.christensen@cox.net>
2017-05-15 04:47:59 +0000

    firehose track 2, lesson 6

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,7 @@
 class QuotesController < ApplicationController
-    # next 2 lines added 14 may 17
+    # next 4 lines added 14 may 17
     def index
+        # @quote = Quote.first
+        @quote = Quote.order("RANDOM()").first
     end
 end
