d40ca6714baaf1971861443b450e820f1e8cc75f
Victor Christensen <victor.christensen@cox.net>
2017-05-17 00:08:32 +0000

    firehose track 2, lesson 13

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,8 +4,20 @@ class QuotesController < ApplicationController
         # @quote = Quote.first
         @quote = Quote.order("RANDOM()").first
     end
-    # next 3 lines added 16 may 17
+
+    # next 11 lines added 16 may 17
     def new
         @quote = Quote.new
     end
+    def create
+        Quote.create(quote_params)
+        redirect_to root_path
+    end
+    
+    private
+    
+    def quote_params
+        params.require(:quote).permit(:saying, :author)
+    end
+
 end
