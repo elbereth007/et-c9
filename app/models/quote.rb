a5e22819a6a49b4ca54d6291cc91542538d9976e
Victor Christensen <victor.christensen@cox.net>
2017-05-17 00:41:30 +0000

    firehose track 2, lesson 14

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ApplicationRecord
+    # next line added 16 may 17
+    validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+    validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
